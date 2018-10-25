41d69210af9023dc77b9bb7f5ffed79c9b3ac696
JaseG256 <jason_r_gibbs@yahoo.com>
2018-10-25 10:59:12 -0700

    RoleRepository unit tested

--- community-servlet/community-servlet/src/main/java/com/Msa/communityservlet/service/DBFileStorageServiceImpl.java
+++ community-servlet/community-servlet/src/main/java/com/Msa/communityservlet/service/DBFileStorageServiceImpl.java
@@ -15,7 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.Optional;
 
-@Service
+@Service(value = "fileServicer")
 public class DBFileStorageServiceImpl implements DBFileStorageService {
 
     @Autowired
--- community-servlet/community-servlet/src/main/java/com/Msa/communityservlet/service/RoleServiceImpl.java
+++ community-servlet/community-servlet/src/main/java/com/Msa/communityservlet/service/RoleServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@Service(value = "roleServicer")
 public class RoleServiceImpl implements RoleService{
 
     @Autowired
