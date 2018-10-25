a70a96ef9b28cfcdbcb273167a8771e092ceaa9f
JaseG256 <jason_r_gibbs@yahoo.com>
2018-10-25 09:06:30 -0700

    User component added

--- community-ui/src/app/app.module.ts
+++ community-ui/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
