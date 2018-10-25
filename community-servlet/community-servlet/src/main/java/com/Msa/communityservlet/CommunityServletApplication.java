2224929c624d2e5601d705e17f945f957ea27213
JaseG256 <jason_r_gibbs@yahoo.com>
2018-10-25 09:47:59 -0700

    Initial Commit

---
+++ community-servlet/community-servlet/src/main/java/com/Msa/communityservlet/CommunityServletApplication.java
@@ -0,0 +1,12 @@
+package com.Msa.communityservlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityServletApplication.class, args);
+	}
+}
---
+++ community-servlet/community-servlet/src/test/java/com/Msa/communityservlet/CommunityServletApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Msa.communityservlet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommunityServletApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
