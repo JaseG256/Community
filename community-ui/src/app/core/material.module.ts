c83f968336d853a350a9c851dd523a026e48c1db
JaseG256 <jason_r_gibbs@yahoo.com>
2018-10-25 14:54:55 -0700

    Angular Material Module added

---
+++ community-ui/src/app/core/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatButtonModule, MatToolbarModule, MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule,
+    MatCardModule, MatProgressSpinnerModule, MatDialogModule } from '@angular/material';
+import { from } from "rxjs";
+
+
+@NgModule({
+    imports: [CommonModule, MatButtonModule, MatToolbarModule, MatInputModule, MatTableModule, MatPaginatorModule,
+         MatSortModule, MatCardModule, MatProgressSpinnerModule, MatDialogModule],
+    exports: [CommonModule, MatButtonModule, MatToolbarModule, MatInputModule, MatTableModule, MatPaginatorModule,
+        MatSortModule, MatCardModule, MatProgressSpinnerModule, MatDialogModule]
+})
+export class CustomMaterialModule {
+
+}
