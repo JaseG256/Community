19f6becca4cc21f90756c59c864de023a65a6f73
JaseG256 <jason_r_gibbs@yahoo.com>
2018-10-25 15:26:37 -0700

    UserService added

--- community-ui/src/app/user/user.component.ts
+++ community-ui/src/app/user/user.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { User } from '../models/user.model';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  displayedColumns = ['id', 'username', 'email'];
+  dataSource = new MatTableDataSource<User>();
+  users: User[];
+  selectedUser: User;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getUsers()
+    .subscribe(data => {
+      this.users = data;
+    });
+    this.userService.getUsers()
+    .subscribe(data => {
+      this.dataSource.data = data;
+    });
+  }
+
+  onSelect(user: User) {
+    this.selectedUser = user;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  showDetails(row: any) { }
+
+  deleteUser(user: User) {
+    this.userService.deleteUser(user)
+    .subscribe(data => {
+      this.users = this.users.filter(u => u !== user);
+    });
   }
 
 }
