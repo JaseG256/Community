3c7c88440558772e5f048c6482271dae08a6eeab
JaseG256 <jason_r_gibbs@yahoo.com>
2018-10-25 10:09:22 -0700

    UserService added

---
+++ community-servlet/community-servlet/src/main/java/com/Msa/communityservlet/service/UserService.java
@@ -0,0 +1,21 @@
+package com.Msa.communityservlet.service;
+
+import com.Msa.communityservlet.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService extends CRUDService<User> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    List<User> findByIdIn(List<Long> userIds);
+
+    User findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
