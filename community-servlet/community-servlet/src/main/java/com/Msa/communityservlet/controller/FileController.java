161ec2687be1657b6c176b081b9a19a05a9eb916
JaseG256 <jason_r_gibbs@yahoo.com>
2018-10-25 14:30:04 -0700

    User Controller added

---
+++ community-servlet/community-servlet/src/main/java/com/Msa/communityservlet/controller/FileController.java
@@ -0,0 +1,63 @@
+package com.Msa.communityservlet.controller;
+
+import com.Msa.communityservlet.model.DBFile;
+import com.Msa.communityservlet.payload.response.UploadFileResponse;
+import com.Msa.communityservlet.security.CurrentUser;
+import com.Msa.communityservlet.security.UserPrincipal;
+import com.Msa.communityservlet.service.DBFileStorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class FileController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    @Autowired
+    @Qualifier("fileServicer")
+    private DBFileStorageService dbFileStorageService;
+
+    @PreAuthorize("hasRole('USER')")
+    @PostMapping("/uploadFile")
+    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file, @CurrentUser UserPrincipal currentUser) {
+        DBFile dbFile = dbFileStorageService.storeFile(file, currentUser);
+
+        String fileDownLoadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(dbFile.getId())
+                .toUriString();
+
+        return new UploadFileResponse(dbFile.getFileName(), fileDownLoadUri,
+                file.getContentType(), file.getSize());
+    }
+
+//    @PostMapping("/uploadMultipleFiles")
+//    public List<UploadFileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+//        return Arrays.asList(files)
+//                .stream()
+//                .map(file -> uploadFile(file))
+//                .collect(Collectors.toList());
+//    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/downloadFile/{fileId}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) {
+        DBFile dbFile = dbFileStorageService.getFile(fileId);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(dbFile.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename =\"" + dbFile.getFileName() + "\"")
+                .body(new ByteArrayResource(dbFile.getData()));
+    }
+}
---
+++ community-servlet/community-servlet/src/main/java/com/Msa/communityservlet/controller/UserController.java
@@ -0,0 +1,89 @@
+package com.Msa.communityservlet.controller;
+
+import com.Msa.communityservlet.model.User;
+import com.Msa.communityservlet.payload.UserIdentityAvailability;
+import com.Msa.communityservlet.payload.UserSummary;
+import com.Msa.communityservlet.security.CurrentUser;
+import com.Msa.communityservlet.security.UserPrincipal;
+import com.Msa.communityservlet.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@RestController
+@RequestMapping("/api")
+//@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    @Qualifier("userServicer")
+    private UserService userService;
+
+    @PostMapping(path = "/user/add")
+    public User create(@RequestBody User user){
+        return userService.saveOrUpdate(user);
+    }
+
+    //    @PreAuthorize("hasRole('USER')")
+    @Secured("ROLE_USER")
+    @GetMapping("/user/me")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(),
+                currentUser.getAvatar().getData());
+        return userSummary;
+    }
+
+    //    @PreAuthorize("hasRole('USER')")
+    @GetMapping(path = "/user/{id}")
+    public Optional<User> findOne(@PathVariable("id") Long id){
+        return userService.getById(id);
+    }
+
+    @GetMapping(path="/users/{username}")
+    public User findByUserName(@RequestParam("username") String userName) {
+        return userService.findByUsername(userName);
+    }
+
+    @GetMapping(path = "users/email/{email}")
+    public Optional<User> findByEmail(@RequestParam("email") String email) {
+        return userService.findByEmail(email);
+    }
+
+    @PutMapping(path = "/{id}")
+    public User update(@PathVariable("id") Long id, @RequestBody User user){
+        user.setId(id);
+        return userService.saveOrUpdate(user);
+    }
+
+    @GetMapping("/user/checkUsernameAvailability")
+    public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+        Boolean isAvailable = !userService.existsByUsername(username);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/user/checkEmailAvailability")
+    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !userService.existsByEmail(email);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @DeleteMapping(path ="/user/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        userService.delete(id);
+    }
+
+
+    //    @Secured("ROLE_USER")
+//    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping(path = "/users")
+    public List<User> findAll(){
+        return userService.listAll();
+    }
+}
