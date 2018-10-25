04774b12d24c2b401eb35e267ac91a685f64906f
JaseG256 <jason_r_gibbs@yahoo.com>
2018-10-25 14:06:00 -0700

    Request/Response utility classes added

---
+++ community-servlet/community-servlet/src/main/java/com/Msa/communityservlet/payload/UserSummary.java
@@ -0,0 +1,36 @@
+package com.Msa.communityservlet.payload;
+
+public class UserSummary {
+
+    private Long id;
+    private String username;
+    private byte[] avatarData;
+
+    public UserSummary(Long id, String username, byte[] avatarData) {
+        this.id = id;
+        this.username = username;
+        this.avatarData = avatarData;
+    }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public byte[] getAvatarData() {
+        return avatarData;
+    }
+
+    public void setAvatarData(byte[] avatarData) {
+        this.avatarData = avatarData;
+    }
+}
